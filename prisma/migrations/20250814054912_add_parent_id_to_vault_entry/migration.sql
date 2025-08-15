e3616a5c379545e6d2e573435582528fa123efd5
Okafor-Chiagozie <collincity111@gmail.com>
2025-08-16 00:44:52 +0100

    Switch from PostgreSQL to MongoDB with Prisma

--- prisma/migrations/20250814054912_add_parent_id_to_vault_entry/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "VaultEntry" ADD COLUMN     "parentId" TEXT;
-
--- CreateIndex
-CREATE INDEX "VaultEntry_parentId_idx" ON "VaultEntry"("parentId");
-
--- AddForeignKey
-ALTER TABLE "VaultEntry" ADD CONSTRAINT "VaultEntry_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "VaultEntry"("id") ON DELETE SET NULL ON UPDATE CASCADE;
